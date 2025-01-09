df78b4240120d4aedbf01c632f4e9029e74cebb4
Alexander Karkossa <alexander.karkossa@googlemail.com>
2025-01-09 11:26:08 +0100

    fix(AMPER-813): workaround to remove maxSdk

--- settings-datastore/build.gradle.kts
+++ settings-datastore/build.gradle.kts
@@ -1,7 +1,14 @@
+import com.android.build.gradle.BaseExtension
+
 plugins {
     id("de.charlex.convention.centralPublish")
 }
 
 group = "de.charlex.settings"
-version = "2.0.0-alpha02"
+version = "2.0.0-alpha03"
 description = "Kotlin Multiplatform Settings Datastore"
+
+// TODO https://youtrack.jetbrains.com/issue/AMPER-813
+project.extensions.getByType<BaseExtension>().defaultConfig {
+    maxSdk = null
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,50 +5,17 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        mavenCentral()
-        google()
-        // workaround for https://youtrack.jetbrains.com/issue/KT-51379
-        exclusiveContent {
-            forRepository {
-                ivy("https://download.jetbrains.com/kotlin/native/builds") {
-                    name = "Kotlin Native"
-                    patternLayout {
-
-                        // example download URLs:
-                        // https://download.jetbrains.com/kotlin/native/builds/releases/1.7.20/linux-x86_64/kotlin-native-prebuilt-linux-x86_64-1.7.20.tar.gz
-                        // https://download.jetbrains.com/kotlin/native/builds/releases/1.7.20/windows-x86_64/kotlin-native-prebuilt-windows-x86_64-1.7.20.zip
-                        // https://download.jetbrains.com/kotlin/native/builds/releases/1.7.20/macos-x86_64/kotlin-native-prebuilt-macos-x86_64-1.7.20.tar.gz
-                        listOf(
-                            "macos-x86_64",
-                            "macos-aarch64",
-                            "osx-x86_64",
-                            "osx-aarch64",
-                            "linux-x86_64",
-                            "windows-x86_64",
-                            "iosX64"
-                        ).forEach { os ->
-                            listOf("dev", "releases").forEach { stage ->
-                                artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
-                            }
-                        }
-                    }
-                    metadataSources { artifact() }
-                }
-            }
-            filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
-        }
-    }
-}
+include(":sample-android")
+include(":sample-ios")
+include(":sample-shared")
+include(":settings-datastore")
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.2.2")
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
 }
