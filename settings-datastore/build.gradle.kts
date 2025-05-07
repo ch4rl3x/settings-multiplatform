e93fa451be1c11322b6383b8dde7f9be68242777
Alexander Karkossa <alexander.karkossa@googlemail.com>
2025-05-07 11:54:37 +0200

    build: remove minify

--- settings-datastore/build.gradle.kts
+++ settings-datastore/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     id("de.charlex.convention.centralPublish")
 }
 
+android {
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
 group = "de.charlex.settings"
-version = "2.0.0-alpha04"
+version = "2.0.0-alpha05"
 description = "Kotlin Multiplatform Settings Datastore"
